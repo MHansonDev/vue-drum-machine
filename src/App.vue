3fd7ca59182892aa3eb2d17238bbe57316dd5eb9
MHansonDev <mhanson048@gmail.com>
2022-09-07 19:51:56 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Caps />
+  </div>
+</template>
+
+<script>
+import Caps from './components/Caps.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Caps
+  }
+}
+</script>
